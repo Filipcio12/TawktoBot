89dcc51cd61242f5692333900731295eb13a7f87
Filipcio12 <121309536+Filipcio12@users.noreply.github.com>
2023-07-11 18:35:04 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import pyautogui as pag
+import time
+
+time.sleep(3)
+"""res = pag.locateCenterOnScreen("testing_bot/flag.png", confidence=0.8)
+
+if res is not None:
+    pag.moveTo(res)
+else:
+    print("Not found")"""
+
+pag.moveTo(1732, 102)
